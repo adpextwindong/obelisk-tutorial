f2424c0a2eb75beb5b9fb636a1bb5e2c88ca1e44
Takumi Crary <tcrary@gmail.com>
2022-04-04 07:43:56 +0100

    Update deltaFirst diagram

--- src/Obelisk/Diagrams/DeltaFirst.hs
+++ src/Obelisk/Diagrams/DeltaFirst.hs
@@ -1,4 +1,6 @@
-{- LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
 module Main where
 
 import Diagrams.Backend.SVG.CmdLine
@@ -10,4 +12,16 @@ grid x y = frame <> lattice
         frame = rect (fromIntegral x) (fromIntegral y) # lw thick
         lattice = centerXY . vcat . map hcat . replicate y . replicate x $ s
 
-main = mainWith (grid 3 3 :: Diagram B)
+main = mainWith ((grid 3 3 <> example) :: Diagram B)
+
+ray@(V2 rx ry) = V2 0.5 0.8
+dfVert = V2 0 rx
+dfHorz = V2 rx 0
+
+drawV v = fromOffsets [v]
+
+example = mconcat
+  [ arrowV ray
+  , translate (V2 rx 0) $ arrowV dfVert # lc red # lw thick
+  , translate (V2 0 0) $ arrowV dfHorz # lc blue # lw thick
+  ]
